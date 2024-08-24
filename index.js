812eb90e6786def6673465d7c580041cb544a013
Rahul Kumar <rks950408@gmail.com>
2024-08-24 17:11:06 +0530

    upddated

---
+++ index.js
@@ -0,0 +1,28 @@
+//fundamental of javascript:
+//arrays and objects
+//function return
+//async js coding
+
+
+var arr=[1,2,3,4,5];
+
+// console.log(arr);
+// arr.forEach(function(val){
+//    console.log(val+"hello") ;
+// })
+
+
+
+
+// .......................4
+
+// map function
+// 
+let ans=arr.map(function(val){
+    return val;
+})
+console.log(ans);
+// .......................5
+// filter function
+
+
